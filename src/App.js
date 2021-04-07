ab1bc7c43dd4701fe4fc55747796ce9a2b19b073
elpuchito <56013454+elpuchito@users.noreply.github.com>
2021-04-06 21:11:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons";
+
+const App = () => {
+  const [articles, setarticles] = useState([]);
+  const [subreddit, setsubreddit] = useState("spacex");
+
+  useEffect(() => {
+    fetch("https://www.reddit.com/r/" + subreddit + ".json").then((res) => {
+      if (res.status != 200) {
+        console.log("error");
+        return;
+      }
+      res.json().then((res) => {
+        if (res != null) {
+          setarticles(res.data.children);
+        }
+      });
+    });
+    return () => {};
+  }, [subreddit]);
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="text"
+          name=""
+          id=""
+          className="search"
+          value={subreddit}
+          onChange={(e) => setsubreddit(e.target.value)}
+        />
       </header>
+      <div className="articles">
+        {articles != null
+          ? articles.map((article, index) => (
+              <Article key={index} article={article.data}></Article>
+            ))
+          : ""}
+      </div>
     </div>
   );
-}
+};
+
+const Article = (props) => {
+  return (
+    <article>
+      <div className="count-container">
+        <FontAwesomeIcon icon={faArrowUp}></FontAwesomeIcon>
+        <p>{props.article.ups}</p>
+        <FontAwesomeIcon icon={faArrowDown}></FontAwesomeIcon>
+      </div>
+      <a href={"https://www.reddit.com" + props.article.permalink}>
+        <p>{props.article.title}</p>
+      </a>
+    </article>
+  );
+};
 
 export default App;
